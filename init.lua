a20fd03cb8b80bd2e7090637d6291a96e6ab5631
Kevin Fu <kevin@liahu.co>
2024-06-12 10:14:03 +0800

    Add more options for telescope

--- init.lua
+++ init.lua
@@ -1,2 +1,28 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- The both meathods can work for ignorecase
+require('telescope').setup{
+  defaults = {
+    file_ignore_patterns = {
+      ".git",
+      "temp",
+      "log",
+      "*.log"
+    },
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--hidden",
+      "--ignore-file",
+      ".gitignore",
+      "--ignore-file",
+      "node-modules",
+      "--smart-case",
+    },
+  }
+}
